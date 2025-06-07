1b63f2d99b27b29a33952ddb8abfcb5c70687ff3
daniel-ibok <etimdaniel08@gmail.com>
2025-06-07 09:22:44 +0100

    made connection with database

--- database/database.go
+++ database/database.go
@@ -1,6 +1,11 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 type Database struct {
 	DB *gorm.DB
@@ -9,4 +14,16 @@ type Database struct {
 type Server struct {
 	ID       int    `gorm:"primaryKey"`
 	Hostname string `gorm:"string;not null,index:idx_name,unique"`
+}
+
+// connectToDB establishes a connection to the PostgreSQL database using GORM.
+func Connect(dsn string) (*Database, error) {
+	if dsn == "" {
+		return nil, gorm.ErrInvalidDB
+	}
+
+	fmt.Println(dsn)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&Server{}) // auto-migrate to database
+	return &Database{DB: db}, err
 }
