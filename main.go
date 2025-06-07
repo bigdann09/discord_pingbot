524974535ebf64270a16c901b0828c580b9c3a0f
daniel-ibok <etimdaniel08@gmail.com>
2025-06-07 09:24:04 +0100

    settip up config and database in min.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"pingbot/config"
+	"pingbot/database"
+)
 
+func main() {
+	// load config
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	// connect to the database
+	db, err := database.Connect(cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal("failed to connect to database", err)
+	}
+
+	fmt.Println(db)
 }
