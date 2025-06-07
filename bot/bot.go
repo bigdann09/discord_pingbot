927e2e9235486b079784efd7af103955c2a9d84f
daniel-ibok <etimdaniel08@gmail.com>
2025-06-07 09:45:31 +0100

    handling command to add/persist hostname to database

--- bot/bot.go
+++ bot/bot.go
@@ -3,9 +3,11 @@ package bot
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"os/signal"
 	"pingbot/database"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -51,10 +53,49 @@ func (bot *Bot) Run() {
 			response := "Pong! The bot is alive."
 			bot.session.ChannelMessageSend(m.ChannelID, response)
 
+		} else if strings.Contains(m.Content, "!add") {
+
+			// Extract the hostname from the message
+			content := strings.Split(m.Content, " ")
+			if len(content) < 2 {
+				bot.session.ChannelMessageSend(m.ChannelID, "Please provide a hostname to add. Usage: `!add <hostname>`")
+				return
+			}
+
+			hostname := content[1]
+			fmt.Println("Adding server:", hostname)
+			if err := bot.add(hostname); err != nil {
+				log.Println("Failed to add server:", err)
+				bot.session.ChannelMessageSend(m.ChannelID, err.Error())
+				return
+			}
+
+			log.Printf("Server %s added successfully.\n", hostname)
+			bot.session.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Server %s added successfully.", "hostname"))
+
 		}
 	})
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
+}
+
+// add hostname to database
+func (bot *Bot) add(hostname string) error {
+	if hostname == "" {
+		return fmt.Errorf("please provide a hostname to add")
+	}
+
+	// lookup host
+	if _, err := net.LookupHost(hostname); err != nil {
+		return fmt.Errorf("failed to resolve hostname %s: %v", hostname, err)
+	}
+
+	// Add the server to the database
+	if err := bot.db.AddServer(hostname); err != nil {
+		return fmt.Errorf("failed to add server: %v", err)
+	}
+
+	return nil
 }
